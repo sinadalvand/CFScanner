6c91d241f96dc2cec5f65f1ff13c8beae924f971
OutOfCode <0ut0fCode@gmail.com>
2023-04-18 00:02:09 +0330

    Add Default Config

--- app/src/main/java/ir/filternet/cfscanner/utils/AppConfig.kt
+++ app/src/main/java/ir/filternet/cfscanner/utils/AppConfig.kt
@@ -86,4 +86,10 @@ object AppConfig {
     const val MSG_MEASURE_CONFIG = 7
     const val MSG_MEASURE_CONFIG_SUCCESS = 71
     const val MSG_MEASURE_CONFIG_CANCEL = 72
+
+
+    const val APPLICATION_REPOSITORY = "https://github.com/MortezaBashsiz/CFScanner/releases/"
+    const val CIDR_Address = "https://raw.githubusercontent.com/MortezaBashsiz/CFScanner/dev/config/cf.local.iplist"
+    const val Config_Address = "https://raw.githubusercontent.com/MortezaBashsiz/CFScanner/main/bash/ClientConfig.json"
+    const val YANDEX_METRICA_KEY = "6fae7880-b925-45d4-9162-891d485a3173"
 }
