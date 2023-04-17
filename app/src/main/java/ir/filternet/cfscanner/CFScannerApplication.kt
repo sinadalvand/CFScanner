eb3bbd5ba396f47da7b1277693c0435d3c83bb8b
OutOfCode <0ut0fCode@gmail.com>
2023-04-18 00:00:06 +0330

    add crash reporter

--- app/src/main/java/ir/filternet/cfscanner/CFScannerApplication.kt
+++ app/src/main/java/ir/filternet/cfscanner/CFScannerApplication.kt
@@ -2,6 +2,8 @@ package ir.filternet.cfscanner
 
 import android.app.Application
 import android.content.Context
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import dagger.hilt.android.HiltAndroidApp
 import go.Seq
 import ir.filternet.cfscanner.db.CFSDatabase
@@ -10,6 +12,7 @@ import ir.filternet.cfscanner.db.dao.ConfigDao
 import ir.filternet.cfscanner.db.dao.ConnectionDao
 import ir.filternet.cfscanner.db.dao.ISPDao
 import ir.filternet.cfscanner.offline.TinyStorage
+import ir.filternet.cfscanner.utils.AppConfig
 import ir.filternet.cfscanner.utils.userAssetPath
 import libv2ray.Libv2ray
 import okhttp3.OkHttpClient
@@ -40,5 +43,9 @@ class CFScannerApplication : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+
+        val config = YandexMetricaConfig.newConfigBuilder(AppConfig.YANDEX_METRICA_KEY).build()
+        YandexMetrica.activate(this, config)
+        YandexMetrica.enableActivityAutoTracking(this)
     }
 }
