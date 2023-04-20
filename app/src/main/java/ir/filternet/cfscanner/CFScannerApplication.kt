180ce9ade367ca55939692e989cfe9df489ff8c8
Sina <dalvandsina@yahoo.com>
2023-04-21 01:54:16 +0330

    increase version

--- app/src/main/java/ir/filternet/cfscanner/CFScannerApplication.kt
+++ app/src/main/java/ir/filternet/cfscanner/CFScannerApplication.kt
@@ -1,26 +1,15 @@
 package ir.filternet.cfscanner
 
 import android.app.Application
-import android.content.Context
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
-import com.lib.remotelogger.logutility.RemoteLoggerBuilder
-import com.yandex.metrica.ReporterConfig
 import com.yandex.metrica.YandexMetrica
 import com.yandex.metrica.YandexMetricaConfig
 import dagger.hilt.android.HiltAndroidApp
 import go.Seq
-import ir.filternet.cfscanner.db.CFSDatabase
-import ir.filternet.cfscanner.db.dao.CIDRDao
-import ir.filternet.cfscanner.db.dao.ConfigDao
-import ir.filternet.cfscanner.db.dao.ConnectionDao
-import ir.filternet.cfscanner.db.dao.ISPDao
-import ir.filternet.cfscanner.offline.TinyStorage
-import ir.filternet.cfscanner.utils.AppConfig
 import ir.filternet.cfscanner.utils.userAssetPath
 import libv2ray.Libv2ray
-import okhttp3.OkHttpClient
 import timber.log.Timber
 
 @HiltAndroidApp
